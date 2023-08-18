182d2101700165ffb7d5ab200c333326e0c98195
srinivas <srinivasa301297@gmail.com>
2023-08-18 10:23:28 +0530

    class based view

---
+++ blog/views.py
@@ -0,0 +1,7 @@
+from django.views.generic import ListView
+from .models import Post
+
+class PostListView(ListView):
+    model = Post
+    template_name = 'blog/post_list.html' 
+    context_object_name = 'posts'
